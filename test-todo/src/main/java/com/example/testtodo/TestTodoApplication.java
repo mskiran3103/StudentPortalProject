6aa145f034a3c366121f55c13ae14607feddbaf6
mskiran3103 <mskiranusa@gmail.com>
2023-08-28 22:38:37 -0400

    Updating UI folder as test_UI

---
+++ test-todo/src/main/java/com/example/testtodo/TestTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTodoApplication.class, args);
+	}
+
+}
